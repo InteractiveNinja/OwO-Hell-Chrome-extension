1f0b642588064711bbe64e334c0061e144a6da3b
Gabriel Nadolny <gabrielnadolny2@gmail.com>
2020-01-21 09:39:31 +0100

    Working Version

--- bg.js
+++ bg.js
@@ -1,24 +1,24 @@
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: '<all_urls>' },
-            })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
-});
+// chrome.runtime.onInstalled.addListener(function () {
+//     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+//         chrome.declarativeContent.onPageChanged.addRules([{
+//             conditions: [new chrome.declarativeContent.PageStateMatcher({
+//                 pageUrl: { hostEquals: '<all_urls>' },
+//             })
+//             ],
+//             actions: [new chrome.declarativeContent.ShowPageAction()]
+//         }]);
+//     });
+// });
 
 
 
-chrome.tabs.onUpdated.addListener(
-    function (tabId, changeInfo, tab) {
-        // read changeInfo data and do something with it
-        // sends a message to contentScript.js
-        chrome.tabs.sendMessage(tabId, {
-            message: 'urlChanged',
-            url: changeInfo.url
-        });
-    }
-);
+// chrome.tabs.onUpdated.addListener(
+//     function (tabId, changeInfo, tab) {
+//         // read changeInfo data and do something with it
+//         // sends a message to contentScript.js
+//         chrome.tabs.sendMessage(tabId, {
+//             message: 'urlChanged',
+//             url: changeInfo.url
+//         });
+//     }
+// );
--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-$("h1").each(() => {
-    alert($(this).text());
-}) 
 var faces = ["(・`ω´・)", ";;w;;", "owo", "UwU", ">w<", "^w^"];
-
 function OwoifyText(v) {
     v = v.replace(/(?:r|l)/g, "w");
     v = v.replace(/(?:R|L)/g, "W");
@@ -10,7 +6,76 @@ function OwoifyText(v) {
     v = v.replace(/N([aeiou])/g, 'Ny$1');
     v = v.replace(/N([AEIOU])/g, 'Ny$1');
     v = v.replace(/ove/g, "uv");
-    //v = v.replace(/\!+/g, " " + faces[Math.floor(Math.random() * faces.length)] + " ");
-    console.log(v);
+    v = v.replace(/\!+/g, " " + faces[Math.floor(Math.random() * faces.length)] + " ");
     return v;
 };
+
+$('div').contents().filter(function () {
+    return this.nodeType == 3
+}).each(function () {
+    this.textContent = this.textContent.replace(this.textContent, OwoifyText(this.textContent));
+});
+
+
+$('h1').contents().filter(function () {
+    return this.nodeType == 3
+}).each(function () {
+    this.textContent = this.textContent.replace(this.textContent, OwoifyText(this.textContent));
+
+});
+
+$('h2').contents().filter(function () {
+    return this.nodeType == 3
+}).each(function () {
+    this.textContent = this.textContent.replace(this.textContent, OwoifyText(this.textContent));
+
+});
+
+$('h3').contents().filter(function () {
+    return this.nodeType == 3
+}).each(function () {
+    this.textContent = this.textContent.replace(this.textContent, OwoifyText(this.textContent));
+
+});
+
+$('span').contents().filter(function () {
+    return this.nodeType == 3
+}).each(function () {
+    this.textContent = this.textContent.replace(this.textContent, OwoifyText(this.textContent));
+
+});
+
+$('p').contents().filter(function () {
+    return this.nodeType == 3
+}).each(function () {
+    this.textContent = this.textContent.replace(this.textContent, OwoifyText(this.textContent));
+
+});
+
+
+$('button').contents().filter(function () {
+    return this.nodeType == 3
+}).each(function () {
+    this.textContent = this.textContent.replace(this.textContent, OwoifyText(this.textContent));
+
+});
+
+$('input').contents().filter(function () {
+    return this.nodeType == 3
+}).each(function () {
+    this.textContent = this.textContent.replace(this.textContent, OwoifyText(this.textContent));
+
+});
+
+$('li').contents().filter(function () {
+    return this.nodeType == 3
+}).each(function () {
+    this.textContent = this.textContent.replace(this.textContent, OwoifyText(this.textContent));
+    
+});
+
+$('a').contents().filter(function () {
+    return this.nodeType == 3
+}).each(function () {
+    this.textContent = this.textContent.replace(this.textContent, OwoifyText(this.textContent));
+});
