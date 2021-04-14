bcf8a85ba1941e16e94f3580fbcf3918fba923b1
InteractiveNinja <gabrielnadolny2@gmail.com>
2021-04-14 11:32:38 +0200

    owofied

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-// var faces = ["(・`ω´・)", ";;w;;", "owo", "UwU", ">w<", "^w^"];
-// function OwoifyText(v) {
-//     v = v.replace(/(?:r|l)/g, "w");
-//     v = v.replace(/(?:R|L)/g, "W");
-//     v = v.replace(/n([aeiou])/g, 'ny$1');
-//     v = v.replace(/N([aeiou])/g, 'Ny$1');
-//     v = v.replace(/N([AEIOU])/g, 'Ny$1');
-//     v = v.replace(/ove/g, "uv");
-//     v = v.replace(/\!+/g, " " + faces[Math.floor(Math.random() * faces.length)] + " ");
-//     return v;
-// };
+var faces = ["(・`ω´・)", ";;w;;", "owo", "UwU", ">w<", "^w^"];
+function OwoifyText(v) {
+    v = v.replace(/(?:r|l)/g, "w");
+    v = v.replace(/(?:R|L)/g, "W");
+    v = v.replace(/n([aeiou])/g, 'ny$1');
+    v = v.replace(/N([aeiou])/g, 'Ny$1');
+    v = v.replace(/N([AEIOU])/g, 'Ny$1');
+    v = v.replace(/ove/g, "uv");
+    v = v.replace(/\!+/g, " " + faces[Math.floor(Math.random() * faces.length)] + " ");
+    return v;
+};
 
 
 // //Wechselt das Suchlogo
@@ -92,4 +92,10 @@
 //     console.log("Hallo Welt :)")
 // })
 
-console.log("hallo welt")
+
+let elements : NodeListOf<Element> = document.querySelectorAll("p,h1,h2,h3,a");
+
+elements.forEach(e => {
+    e.textContent = OwoifyText(e.textContent);
+})
+
